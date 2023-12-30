77657c15bdbb3a6ebf3ad62e3ce0e02a32820ce6
anandu_170758 <ananduksyam@gmail.com>
2023-12-30 14:54:25 +0530

    MySQL Integrated

--- anandu_project/settings.py
+++ anandu_project/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'anandu_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'shopify',
+        'HOST':'localhost',
+        'USER':'root',
+        'PORT':'3306'
     }
 }
 
