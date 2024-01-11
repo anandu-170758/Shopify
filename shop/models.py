933a8d300fd7537f070d609f988501aa21092763
anandu_170758 <ananduksyam@gmail.com>
2024-01-11 12:15:44 +0530

    Navbar Added

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime
+import os 
 
-# Create your models here.
+def getFileName(request,filename):
+    now_time=datetime.datetime.now().strftime("%Y%m%d%H:%M:%S")
+    new_filename="%s%s"%(now_time,filename)
+    return os.path.join('uploads/',)
+
+class Category(models.model):
+    name=models.CharField(max_length=150,null=False,blank=False)
+    image=models.ImageField(upload_to=getFileName)
