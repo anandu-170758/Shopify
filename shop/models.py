a45c33a10b4c31f4ba7da4e17c9cdfbe9a06c0af
anandu_170758 <ananduksyam@gmail.com>
2024-01-11 22:16:36 +0530

    Pillow Added

--- shop/models.py
+++ shop/models.py
@@ -7,6 +7,28 @@ def getFileName(request,filename):
     new_filename="%s%s"%(now_time,filename)
     return os.path.join('uploads/',)
 
-class Category(models.model):
+class Category(models.Model):
     name=models.CharField(max_length=150,null=False,blank=False)
-    image=models.ImageField(upload_to=getFileName)
+    image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+    description=models.TextField(max_length=500,null=False,blank=False)
+    status=models.BooleanField(default=False,help_text="0-default,Hidden")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    Category=models.ForeignKey(Category,on_delete=models.CASCADE)
+    name=models.CharField(max_length=150,null=False,blank=False)
+    vendor=models.CharField(max_length=150,null=False,blank=False)
+    product_image=models.ImageField(upload_to=getFileName,null=True,blank=True)
+    quantity=models.IntegerField(null=False,blank=False)
+    orginal_price=models.FloatField(null=False,blank=False)
+    selling_price=models.FloatField(null=False,blank=False)
+    description=models.TextField(max_length=500,null=False,blank=False)
+    status=models.BooleanField(default=False,help_text="0-default,Hidden")
+    trending=models.BooleanField(default=False,help_text="0-default,1-Trending")
+    created_at=models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
