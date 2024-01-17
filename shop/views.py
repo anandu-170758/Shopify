66039677fe76e4cd558c322f33b810504b6399cb
anandu_170758 <ananduksyam@gmail.com>
2024-01-18 02:00:04 +0530

    Collection Page Created

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from . models import*
 def home(request):
     return render(request,"shop/index.html")
 def register(request):
     return render(request,"shop/register.html")
+def collections(request):
+    category=Category.objects.filter(status=0)
+    return render(request,"shop/collections.html",{"category":category})
