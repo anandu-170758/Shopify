4d0061f5a50247f09ac317fe8204289f1c918381
anandu_170758 <ananduksyam@gmail.com>
2024-01-20 12:23:29 +0530

    Path issue Solved

--- shop/views.py
+++ shop/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from . models import*
+from django.contrib import messages
+
+
 def home(request):
     return render(request,"shop/index.html")
 def register(request):
     return render(request,"shop/register.html")
 def collections(request):
     category=Category.objects.filter(status=0)
-    return render(request,"shop\collections.html",{"category":category})
+    return render(request,"shop/collections.html",{"category":category})
+def collectionsview(request,name):
+    if (Category.objects.filter(name=name,status=0)):
+        products=Product.objects.filter(category__name=name)
+        return render(request,"shop\Products\index.html",{"products":products})
+    else:
+        messages.warning(request,"No Such Category Found")
+        return redirect('collections')
+      
