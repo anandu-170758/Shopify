f450636856a81da15eec9171311a8ab80d07e894
anandu_170758 <ananduksyam@gmail.com>
2024-01-24 02:10:32 +0530

    product details image routed

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ urlpatterns=[
     path('register',views.register,name="register"),
     path('collections',views.collections,name="collections"),
     path('collections/<str:name>',views.collectionsview,name="collections"),
+    path('collections/<str:cname>/<str:pname>',views.product_details,name="product_details"),
  ]
