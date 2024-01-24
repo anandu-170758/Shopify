0f0bb3c21db6100cd3f265839b1805577aa5a0fa
anandu_170758 <ananduksyam@gmail.com>
2024-01-25 01:44:50 +0530

    Add to Cart Page

--- shop/views.py
+++ shop/views.py
@@ -2,14 +2,28 @@ from django.shortcuts import render,redirect
 from shop.form import CustomUserForm
 from . models import*
 from django.contrib import messages
-
+from django.contrib.auth import authenticate,login,logout
 
 def home(request):
   products=Product.objects.filter(trending=1)
   return render(request,"shop/index.html",{"products":products})
 
-def login(request):
-  return render(request,"shop/login.html",)
+def login_page(request):
+  if request.user.is_authenticated:
+    return redirect("/")
+  else:
+    if request.method=='POST':
+      name=request.POST.get('username')
+      pwd=request.POST.get('password')
+      user=authenticate(request,username=name,password=pwd)
+      if user is not None:
+        login(request,user)
+        messages.success(request,"Logged in Successfully")
+        return redirect("/")
+      else:
+        messages.error(request,"Invalid User Name or Password")
+        return redirect("/login")
+    return render(request,"shop/login.html")
 
 def register(request):
     form=CustomUserForm
@@ -44,3 +58,9 @@ def product_details(request,cname,pname):
     else:
       messages.error(request,"No Such Catagory Found")
       return redirect('collections')
+    
+def logout_page(request):
+  if request.user.is_authenticated:
+    logout(request)
+    messages.success(request,"Logged out Successfully")
+  return redirect("/") 
