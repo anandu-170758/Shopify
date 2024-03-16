1aae266ac53f3aefca2caeeae3b8936c14571195
anandu_170758 <ananduksyam@gmail.com>
2024-03-16 18:33:45 +0530

    vercel config

--- anandu_project/settings.py
+++ anandu_project/settings.py
@@ -117,6 +117,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL='/images/'
 MEDIA_ROOT=BASE_DIR/'static'
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
 
 STATICFILES_DIRS=[
     BASE_DIR/'static'
